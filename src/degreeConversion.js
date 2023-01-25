2d5daa780065edc41f5f84aa8e33f29f75a16c61
olgakahraman <ol.khrmn@gmail.com>
2023-01-25 23:08:28 +0300

    Update degreeConversion.js

--- src/degreeConversion.js
+++ src/degreeConversion.js
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
 
 export default function degreeConversion(props) {
-
   let [temperature, setTemperature] = useState(props.temperature);
   let [unit, setUnit] = useState("Metric");
-
 
   function showFahrenheit(event) {
     event.prevenDefault();
     setUnit("Imperial");
-    setTemperature(Math.round((props.temperature * 9)/5 + 32));
+    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
   }
 
-function showCelsius(event) {
-  event.prevenDefault();
-  setUnit("Metric");
-  setTemperature(props.temperature);
-}
+  function showCelsius(event) {
+    event.prevenDefault();
+    setUnit("Metric");
+    setTemperature(props.temperature);
+  }
 
   return (
     <p>
       Weather in {props.city} is {temperature} ({unit}){" "}
       <a href="/" onClick={showCelsius}>
         ℃
-        </a>{" "}
-        | {" "}
-        <a href="/" onClick={showFahrenheit}>
-          ℉
-        </a>
-        </p>
+      </a>{" "}
+      |{" "}
+      <a href="/" onClick={showFahrenheit}>
+        ℉
+      </a>
+    </p>
   );
 }
