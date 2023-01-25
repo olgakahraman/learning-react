acd7737c02c303f257077c1fc5d4f445a9fb335b
olgakahraman <ol.khrmn@gmail.com>
2023-01-25 16:18:50 +0300

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,11 @@
 import React from "react";
 export default function Forecast() {
-  function seeForecast(event) {
+  function showForecast(event) {
     event.preventDefault();
     alert("Forecast feature is not ready yet");
   }
   return (
-    <a href="/" onClick={seeForecast}>
+    <a href="/" onClick={showForecast}>
       See forecast
     </a>
   );
