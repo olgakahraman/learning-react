9b2e4b7c85fa0a20b0f6dfb52fe3603e76ed6550
olgakahraman <ol.khrmn@gmail.com>
2023-01-26 14:56:55 +0300

    SearchEngine event onChange displays {output}

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 export default function SearchEngine() {
     let [city, setCity] = useState(" ");
+    let [output, setOutput] = useState(" ");
 
     function handleSubmit(event){
         event.preventDefault();
-        alert(`Searching for ${city}`);
+        setOutput(`It is currently 19℃ in ${city}`);
     }
     function updateCity(event){
         setCity(event.target.value);
     }
     return(
+        <div className="SearchEngine">
         <form onSubmit={handleSubmit}>
-        <input type = "search" onChange = {updateCity} />
+        <input type = "search" placeholder="Type a city" onChange = {updateCity} />
         <input type = "submit" value = "Search" />
         </form>
+        <h2>{output}</h2>
+        </div>
     )
 }
